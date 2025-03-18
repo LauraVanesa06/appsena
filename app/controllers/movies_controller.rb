08eee551dca23be8e15caeb95c9264cb65c4ea55
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2025-03-18 18:23:26 -0500

    ...

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,14 @@
 class MoviesController < ApplicationController
   def show
-    @mostrar = Movie.find(1)
-  end
+    movies = Movie.all
+    movies.each do |movie|
+      puts movie.nombre
+    end
+  
 
   def delete
-    @eliminar = Movie.find(1)
-    @eliminar.destroy
+    @eliminar = Movie.find(params[:id])  # Busca la película por ID
+    @eliminar.destroy  # Elimina la película
+    redirect_to movies_path, notice: "Película eliminada exitosamente"
   end
- 
 end
