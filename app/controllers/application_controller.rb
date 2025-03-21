a1ec4785eadae70bad1a3b114247f55f7de9d5c9
LauraVanesa06 <168228452+LauraVanesa06@users.noreply.github.com>
2025-03-20 23:32:54 -0500

    ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  include Devise::Controllers::Helpers  # A
+  before_action :authenticate_user!  # Protege rutas para usuarios autenticados
+
+  # Permitir métodos de Devise en todas las vistas
+  helper_method :user_signed_in?, :current_user
 end
+
+
